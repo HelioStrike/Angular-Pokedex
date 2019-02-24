1ba18ee3615c5e28749a13f02a9342659d1baea9
HelioStrike <rageofepicfury@gmail.com>
2019-02-24 15:05:33 +0530

    committed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyD6Ao-DY7BQY2Fgie53FkM06ID3zAUUqGc",
-    authDomain: "angular-pokedex-b115b.firebaseapp.com",
-    databaseURL: "https://angular-pokedex-b115b.firebaseio.com",
-    projectId: "angular-pokedex-b115b",
-    storageBucket: "angular-pokedex-b115b.appspot.com",
-    messagingSenderId: "995839477522"
+    apiKey: "XXXXXX",
+    authDomain: "XXXXXX.firebaseapp.com",
+    databaseURL: "https://XXXXXX.firebaseio.com",
+    projectId: "XXXXXX",
+    storageBucket: "XXXXXX.appspot.com",
+    messagingSenderId: "XXXXXX"
   }
 };
