21cc8f80779c4c03e3404c9ac724694e5f746d5f
Sai Sandeep Mutyala <rageofepicfury@gmail.com>
2019-03-25 08:31:48 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "XXXXXX",
-    authDomain: "XXXXXX.firebaseapp.com",
-    databaseURL: "https://XXXXXX.firebaseio.com",
-    projectId: "XXXXXX",
-    storageBucket: "XXXXXX.appspot.com",
-    messagingSenderId: "XXXXXX"
+    XXX
   }
 };
 
